d31092a5387d7802bfeceebc06615e73e6f3394d
Marijus <m.jenulis@gmail.com>
2019-07-25 15:59:25 +0300

    added cart page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Route,Switch} from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 import Wishlist from './containers/Wishlist/Wishlist';
+import Cart from './containers/Cart/Cart';
 import Items from './containers/Items/Items';
 import Register from './containers/Auth/Register/Register';
 import Login from './containers/Auth/Login/Login';
@@ -22,6 +23,7 @@ class App extends Component {
             <Route path='/registracija' component={Register}/>
             <Route path='/prisijungti' component={Login}/>
             <Route path='/patikusios-prekes' component={Wishlist}/>
+            <Route path='/prekiu-krepselis' component={Cart}/>
             <Route path='/:category' render={(props) => (<Items key={props.match.params.category} {...props}/>)}/>
             <Route path='/' exact component={Home}/>
           </Switch>
---
+++ client/src/containers/Cart/Cart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Item from '../../components/Item/Item';
+import { connect } from 'react-redux';
+
+import classes from './Cart.module.css';
+
+class Cart extends React.Component {
+    render() {
+        const items = this.props.cart.map(item => (
+            <Item key={item._id} item={item}/>
+        ));
+        return(
+            <div className={classes.Wrapper}>
+                {items}
+            </div>
+        );
+    }
+}
+const mapStateToProps = state => {
+    return {
+        cart: state.items.cart
+    }
+}
+
+export default connect(mapStateToProps)(Cart);
